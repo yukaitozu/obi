ce201f4e8c8837aeecbccd068bf57d283a686af1
yuichiro watanabe <yuuichiro1139@gmail.com>
2020-02-25 12:54:15 +0900

    delete seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-  user.create!(
-    first_name: Faker::Name.first_name
-    last_name: Faker::Name.last_name
-    profile_info: Faker::Quote.yoda
-    )
-end
