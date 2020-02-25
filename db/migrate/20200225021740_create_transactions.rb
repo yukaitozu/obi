a2fdefd5fadfb09bea7619f6bcc8509cce4f1825
Yuka Itozu <yukaitozu@gmail.com>
2020-02-25 11:20:28 +0900

    created transaction model

---
+++ db/migrate/20200225021740_create_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :transactions do |t|
+      t.date :start_date
+      t.date :return_date
+      t.boolean :approved
+      t.references :user, foreign_key: true
+      t.references :listing, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
